39ea84b3992d173cbcc18a3537e61a2170f5007f
李轩宇 <747507469@qq.com>
2020-09-04 17:25:09 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Todos from '../views/Todos.vue'
 
 Vue.use(VueRouter)
 
@@ -8,21 +9,20 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children:[
+        {
+            path:'/:tag',
+            name:'Todos',
+            component:Todos
+        }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+//   mode: 'hash',
+//   base: process.env.BASE_URL,
   routes
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="main">
+      <div id="title">todos</div>
+      <div id="section">
+        <Edit />
+        <Todo v-if="$store.state.todoList.length>0" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Edit from "../components/Edit";
+import Todo from "../components/Todo";
 export default {
-  name: 'Home',
+  name: "App",
   components: {
-    HelloWorld
+    Edit,
+    Todo,
+  },
+  created() {
+    if (window.localStorage.getItem("todos")) {
+      this.$store.dispatch(
+        "changeTodoList",
+        JSON.parse(window.localStorage.getItem("todos"))
+      );
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  #main {
+    width: 550px;
+    #title {
+      font-size: 100px;
+      font-weight: 100;
+      color: #ead7d7;
+    }
+    #section {
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+    }
   }
 }
-</script>
+</style>
